1f0600387b2c17957bf4e5f259a8153c839ba1ad
Thomas Kono <konox006@umn.edu>
2018-08-07 16:25:08 -0500

    Remove unnecessary method from BulkRNASeqPipeline

--- GopherPipelines/Pipelines/BulkRNAseq.py
+++ GopherPipelines/Pipelines/BulkRNAseq.py
@@ -11,17 +11,6 @@ class BulkRNAseqPipeline(Pipeline.Pipeline):
     # Define the pipeline name here
     pipe_name = 'bulk_rnaseq'
 
-    def __repr__(self):
-        """Return a string that can be printed nicely for debugging purposes.
-        The user shouldn't need to see this, but developers will probably
-        use it."""
-        s = '\n'.join([
-            'Pipeline: ' + self.pipe_name,
-            'FASTQ Dir: ' + str(self.fq_dir),
-            'Output Dir: ' + str(self.outdir),
-            'User options: ' + str(self.useropts)])
-        return s
-
     def __init__(self, args):
         """Initialize the pipeline object. We will call the general
         Pipeline.__init__() here, as well as set some specific pipeline
