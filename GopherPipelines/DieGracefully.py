4d6aba17019d04d8a5223929ca0977ead79ca783
Thomas Kono <konox006@umn.edu>
2018-09-13 13:32:06 -0500

    typo

--- GopherPipelines/DieGracefully.py
+++ GopherPipelines/DieGracefully.py
@@ -87,7 +87,7 @@ Samplesheet: {ss}
 Verify the information in the samplesheet, and run the pipeline script with
 bash while logged into Mesabi. You will recieve email notifications of job
 start/completion/error at your UMN X500 email address. If you need to submit
-an error repor, please contact help[at]msi.umn.edu. Please include the
+an error report, please contact help[at]msi.umn.edu. Please include the
 samplesheet, pipeline script, and the error message with your report.\n"""
     sys.stderr.write(msg.format(pn=pipe_script, ss=samplesheet))
     return
