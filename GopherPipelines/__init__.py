1867234e5a8a33fd73c8d480527294add59eb3cf
Thomas Kono <konox006@umn.edu>
2019-03-13 10:24:12 -0500

    Add a time stamp to the default directory

--- GopherPipelines/__init__.py
+++ GopherPipelines/__init__.py
@@ -9,6 +9,8 @@ import datetime
 import getpass
 
 # Define today's date and the current time
+curr_time = datetime.datetime.now()
 TODAY = datetime.date.isoformat(datetime.datetime.today())
-NOW = datetime.datetime.now().isoformat(sep=' ', timespec='seconds')
+TIMESTAMP = str(curr_time.hour) + str(curr_time.minute) + str(curr_time.second)
+NOW = curr_time.isoformat(sep=' ', timespec='seconds')
 UNAME = getpass.getuser()
