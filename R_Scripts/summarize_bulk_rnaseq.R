04baa1b46e2ce9c2d9934f705274e5830625a8d3
Thomas Kono <konox006@umn.edu>
2019-07-05 09:55:52 -0500

    Try to add one sample support

--- R_Scripts/summarize_bulk_rnaseq.R
+++ R_Scripts/summarize_bulk_rnaseq.R
@@ -145,18 +145,25 @@ dev.off()
 # For some reason, sometimes there are fewer than 500 genes that pass filtering
 n_genes <- min(500, nrow(cpm_counts))
 if(n_genes < 500) {
-    write("There are fewer than 500 genes that pass variance filtering fort he clustering heatmap. This is not an error, but you should be aware of it.", stderr())
+    write("There are fewer than 500 genes that pass variance filtering for the clustering heatmap. This is not an error, but you should be aware of it.", stderr())
 }
-gene_var <- apply(cpm_counts, 1, var)
-select_var <- names(sort(gene_var, decreasing=TRUE))[1:n_genes]
-high_var <- cpm_counts[select_var,]
+if(length(samp_ids) > 2) {
+    write("There is only one sample, so we will not try to generate a clustering heatmap. This is not an error.", stderr())
+    pdf(hmap)
+    plot(c(0, 1), c(0, 1), ann=F, bty="n", type="n", xaxt="n", yaxt="n")
+    text(x=0.5, y=0.5, "1 sample;\nClustering heatmap not possible", cex=1, col="black")
+    dev.off()
+} else {
+    gene_var <- apply(cpm_counts, 1, var)
+    select_var <- names(sort(gene_var, decreasing=TRUE))[1:n_genes]
+    high_var <- cpm_counts[select_var,]
 
-# Set the heatmap pdf and plot the normalized counts heatmap
-pdf(hmap)
-heatmap.2(high_var,trace="none", main = paste("Top ", n_genes, " variance genes", sep=""), cexCol = 0.75, dendrogram = "column", labRow = "", ColSideColors = col_vec, srtCol = 45, margins = c(8,8))
-legend('left', title = 'Group', legend = uniq_groups, fill = unique(col_vec), cex = 0.8, box.lty = 0 )
-dev.off()
-
+    # Set the heatmap pdf and plot the normalized counts heatmap
+    pdf(hmap)
+    heatmap.2(high_var,trace="none", main = paste("Top ", n_genes, " variance genes", sep=""), cexCol = 0.75, dendrogram = "column", labRow = "", ColSideColors = col_vec, srtCol = 45, margins = c(8,8))
+    legend('left', title = 'Group', legend = uniq_groups, fill = unique(col_vec), cex = 0.8, box.lty = 0 )
+    dev.off()
+}
 ############################
 # Differential expression testing and summaries
 ############################
