1b027144ae44b5b7676624eca52cb2911670d886
Tom Kono <konox006@umn.edu>
2023-11-27 11:50:39 -0600

    Increment version

--- CHURPipelines/__init__.py
+++ CHURPipelines/__init__.py
@@ -3,8 +3,8 @@
 run analysis on the Minnesota Supercomputing Institute (MSI) cluster."""
 
 # set dunder (double-underscore) variables
-__version__ = '0.3.0-dev'
-__date__ = '2023-08-17'
+__version__ = '0.2.4'
+__date__ = '2023-11-27'
 
 import datetime
 import getpass
