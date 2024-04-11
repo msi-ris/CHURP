4cd937665fbed64c28c5252f6fac2725723676aa
Thomas Kono <konox006@umn.edu>
2024-04-11 17:28:00 -0500

    Remove module load statements from summary stats job

--- PBS/run_summary_stats.sh
+++ PBS/run_summary_stats.sh
@@ -92,9 +92,6 @@ while IFS="|" read -ra OPTS; do
     GTFFILE=${OPTS[12]}
 done <<< "$IN"
 
-# load necessary modules
-module load R/3.5.0
-module load python/3.6.3
 # Make directories for the output files. We want to keep the outdir organized
 LOGDIR="${OUTDIR}/Logs"
 COUNTSDIR="${OUTDIR}/Counts"
